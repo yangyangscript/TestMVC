b580f5db798e44672375bb990c98ac80e85d0671
yangyang <1324954289@qq.com>
2018-11-23 18:47:28 +0800

    动态添加控件

---
+++ TestMVC/Controllers/DynAdd/DynAddDefaultController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppBoxMvc.Controllers.DynAdd
+{
+    public class DynAddDefaultController : Controller
+    {
+        public ActionResult DefaultPage()
+        {
+            return View();
+        }
+    }
+}
