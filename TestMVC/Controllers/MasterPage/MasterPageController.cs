78f1b978b0587311326cd7cf358214bff64d5c58
yangyang <1324954289@qq.com>
2018-11-27 19:01:29 +0800

    到主页面模板,未完成

---
+++ TestMVC/Controllers/MasterPage/MasterPageController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestMVC.ViewModel;
+
+namespace TestMVC.Controllers
+{
+    public partial class MasterPageController : Controller
+    {
+        private List<ViewModel.MasterInfoView> GetDatas(int num)
+        {
+            var now = DateTime.Now;
+            var ret = new List<ViewModel.MasterInfoView>();
+            for (int i = 1; i < num; i++)
+            {
+                var nu1 = i * 2;
+                var nu2 = i * 3;
+                var nu3 = i * 5;
+                var nu4 = i * 7;               
+                ret.Add(new MasterInfoView()
+                {
+                      Name1 = $"Name_{nu1}",
+                      Name2  = $"Name_{nu2}",
+                        Name3 = $"Name_{nu3}",
+                        Name4 = $"Name_{nu4}",
+                      Time1 = now.AddMinutes(-nu1),
+                    Time2 = now.AddMinutes(-nu2),
+                    Time3 = now.AddMinutes(-nu3),
+                    Time4 = now.AddMinutes(-nu4),
+                    Number1 = nu1,
+                    Number2 = nu2,
+                    Number3 = nu3,
+                    Number4 = nu4,
+                });
+            }
+            return ret;
+        }
+    }
+}
---
+++ TestMVC/ViewModel/MasterInfoView.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TestMVC.ViewModel
+{
+    public class MasterInfoView
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "名字一")]
+        public string Name1 { get; set; }
+
+        [Display(Name = "名字二")]
+        public string Name2 { get; set; }
+
+        [Display(Name = "名字三")]
+        public string Name3 { get; set; }
+
+        [Display(Name = "名字四")]
+        public string Name4 { get; set; }
+
+        [Display(Name = "时间一")]
+        public DateTime Time1 { get; set; }
+
+        [Display(Name = "时间二")]
+        public DateTime Time2 { get; set; }
+
+        [Display(Name = "时间三")]
+        public DateTime Time3 { get; set; }
+
+        [Display(Name = "时间四")]
+        public DateTime Time4 { get; set; }
+
+        [Display(Name = "数字一")]
+        public int Number1 { get; set; }
+
+        [Display(Name = "数字二")]
+        public int Number2 { get; set; }
+
+        [Display(Name = "数字三")]
+        public int Number3 { get; set; }
+
+        [Display(Name = "数字四")]
+        public int Number4 { get; set; }
+    }
+}
